0495a2f710a533bb0824eca3c874ea5b29ee5ac0
Hali1202 <congnguyen.31231027857@st.ueh.edu.vn>
2025-04-11 10:33:18 +0700

    3-1 python

--- a.py
+++ a.py
@@ -36,9 +36,64 @@ def ex06():
 	euclidean = math.sqrt((x1-x2)**2 + (y1-y2)**2)
 	print(m)
 	print(euclidean)
+def e07():
+	pass
+def e08():
+	import math
 
+	x = int(input('y = x^2 + 6x + 9 , enter x: '))
+	y = x**2 + 6*x + 2
 
+	print(f'x = {x}, y = {y}')
+	delta = 6*6 - 4*2
+	print(f'delta = {delta}')
 
+	if delta < 0:
+		print('pt vo nghiem')
+	else:
+			if delta > 0:
+				x1 = (-6 + math.sqrt(delta)) / 2
+
+				x2 = (-6 - math.sqrt(delta)) / 2
+
+				print(f'pt co 2 nghiem {x1} va {x2}')
+			else:
+				x0 = -6 / 2
+
+				print(f'pt co nghiem kep x = {x0}')
+def e10():
+	a = input()
+	b = 'on'
+	if b in a:
+		print(f'"on" in {a}')
+	else:
+		print(f'"on" out {a}')
+def e11():
+	a = 'I hope this course is not full of jargon'
+	b = 'jargon'
+	if b in a:
+		print(f'{b} in {a}')
+	else:
+		print(f'{b} out {a}')
+def e13():
+	print(len('python'))
+	print(float(len('python')))
+	print(str(len('python')))
+def e14():
+	a = int(input('enter number: '))
+	if a % 2 == 0:
+		print(f'{a} is even')
+	else:
+		print(f'{a} is odd')
+def e15():
+	a = int(input('enter hours: '))
+	b = int(input('enter rate per hour: '))
+	c == a*b
+
+	print(f'pay = {c}')
+def e16():
+	b = 1 * 365 * 24 * 60 * 60
+	print(f'a person can live in {b} seconds')
 
 if __name__ == '__main__':
-	ex06()
+	ex16()
